dc0caafd6a112ce2f5fdd5314a22e97b923409fa
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-01-24 19:47:56 +0000

    feat: 🎸 Add basic test components
    Proof that this component library idea works.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home () {
-  return <div>Temtem App</div>;
+import React, { useEffect, useState } from "react";
+import TemtemWeaknessTable from "@maael/temtem-weakness-table-component";
+import TemtemCalculator from "@maael/temtem-calculator-component";
+
+export default function Home() {
+  const [types, setTypes] = useState<string[]>([]);
+  const [weaknessTable, setWeaknessTable] = useState({});
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://temtem-api.mael.tech/api/types");
+      if (res.ok) {
+        setTypes((await res.json()).map(({ name }) => name));
+      }
+    })();
+  });
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://temtem-api.mael.tech/api/weaknesses");
+      if (res.ok) {
+        setWeaknessTable(await res.json());
+      }
+    })();
+  });
+  return (
+    <div>
+      <div>Temtem App</div>
+      <TemtemWeaknessTable
+        weaknessData={weaknessTable}
+        renderHeading={t => <p>t</p>}
+      />
+      <TemtemCalculator weaknessData={weaknessTable} types={types} />
+    </div>
+  );
 }
