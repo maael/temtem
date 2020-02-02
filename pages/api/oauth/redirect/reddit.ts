10f3419ead38da86ac06ff0e12f931e2bfbaa939
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-02 17:38:08 +0000

    fix: 🐛 Better error catching on oauth redirect route

--- pages/api/oauth/redirect/reddit.ts
+++ pages/api/oauth/redirect/reddit.ts
@@ -46,42 +46,47 @@ async function getOrCreateUser(identity: any) {
 
 export default cookies(async function(req, res) {
   const { error, code, state } = req.query;
-  if (!error) {
-    if (state === "testing") {
-      const { access_token } = await getAccessToken(code);
-      if (access_token) {
-        const identity = await getIdentity(access_token);
-        const {
-          _id,
-          redditName,
-          redditId,
-          redditDarkmode,
-          redditIcon
-        } = await getOrCreateUser(identity);
-        const toEncode: JWT = {
-          _id,
-          redditId,
-          redditName,
-          redditIcon,
-          redditDarkmode,
-          version: JWT_VERSION
-        };
-        const jwtToken = await jwt(toEncode);
-        res.cookie("temtem-jwt", jwtToken, {
-          expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 90),
-          sameSite: "Lax",
-          path: "/"
-        });
-        res.writeHead(301, { Location: "/" });
-        res.end();
+  try {
+    if (!error) {
+      if (state === "testing") {
+        const { access_token } = await getAccessToken(code);
+        if (access_token) {
+          const identity = await getIdentity(access_token);
+          const {
+            _id,
+            redditName,
+            redditId,
+            redditDarkmode,
+            redditIcon
+          } = await getOrCreateUser(identity);
+          const toEncode: JWT = {
+            _id,
+            redditId,
+            redditName,
+            redditIcon,
+            redditDarkmode,
+            version: JWT_VERSION
+          };
+          const jwtToken = await jwt(toEncode);
+          res.cookie("temtem-jwt", jwtToken, {
+            expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 90),
+            sameSite: "Lax",
+            path: "/"
+          });
+          res.writeHead(301, { Location: "/" });
+          res.end();
+        } else {
+          console.error("[response error]", "missing access_token");
+        }
       } else {
-        console.error("[response error]", "missing access_token");
+        console.error("[state error]", "state mismatch", state, "testing");
       }
     } else {
-      console.error("[state error]", "state mismatch", state, "testing");
+      console.error("[error]", error);
     }
-  } else {
-    console.error("[error]", error);
+  } catch (err) {
+    console.info(">>>>> error", err);
+    res.json({ err });
   }
 });
 
