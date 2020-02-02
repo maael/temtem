2aedbeb48fcec1ca19d00485d573b5e3067dca68
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-02 20:13:39 +0000

    feat: 🎸 Add search, tamed total, and total, to tempedia

--- pages/tempedia.tsx
+++ pages/tempedia.tsx
@@ -2,13 +2,15 @@
 import { useEffect, useState } from "react";
 import { jsx } from "@emotion/core";
 import TemtemText from "@maael/temtem-text-component";
+import TemtemInput from "@maael/temtem-input-component";
 import TemtemPortrait from "@maael/temtem-portrait-component";
 import TemtemButton from "@maael/temtem-button-component";
 import useJWT from "../components/hooks/useJWT";
 
 export default function Tempedia() {
   const jwt = useJWT();
-  const [temtem, setTemtem] = useState([]);
+  const [search, setSearch] = useState("");
+  const [temtem, setTemtem] = useState<any[]>([]);
   const [taming, setTaming] = useState<string[]>([]);
   const [tamed, setTamed] = useState<string[]>([]);
   useEffect(() => {
@@ -49,59 +51,89 @@ export default function Tempedia() {
   }
   return (
     <div style={{ margin: "10px auto", maxWidth: 1200, textAlign: "center" }}>
-      {temtem.map(({ number: num, name, types }) => (
-        <div
-          key={num}
-          css={{
-            display: "inline-block",
-            width: 170
-          }}
-        >
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <TemtemInput
+          containerStyle={{ flex: 1, textAlign: "left" }}
+          value={search}
+          onChange={({ target }) => setSearch(() => (target as any).value)}
+          placeholder="Search..."
+        />
+        {jwt ? (
+          <TemtemText
+            containerStyle={{ margin: 10 }}
+            style={{ fontSize: 20 }}
+            borderWidth={10}
+          >{`Tamed: ${tamed.length}`}</TemtemText>
+        ) : null}
+        <TemtemText
+          containerStyle={{ margin: 10 }}
+          style={{ fontSize: 20 }}
+          borderWidth={10}
+        >{`Total: ${temtem.length}`}</TemtemText>
+      </div>
+      {temtem
+        .filter(({ name }) =>
+          search.trim() ? name.includes(search.trim()) : true
+        )
+        .map(({ number: num, name, types }) => (
           <div
+            key={num}
             css={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column"
+              display: "inline-block",
+              width: 170
             }}
           >
-            <TemtemPortrait
-              style={{ margin: "0 auto" }}
-              temtem={name}
-              shape="hexagon"
-              size={100}
-              borderWidth={5}
-            />
-            <TemtemText
-              style={{ fontSize: 20, textAlign: "center" }}
-              borderWidth={10}
+            <div
+              css={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
             >
-              {`#${num} ${name}`}
-            </TemtemText>
-            {jwt ? (
-              tamed.includes(name) ? (
-                <TemtemText
-                  containerStyle={{ marginTop: 7 }}
-                  style={{ fontSize: 20 }}
-                  borderWidth={10}
-                >
-                  Tamed
-                </TemtemText>
-              ) : (
-                <TemtemButton
-                  size="small"
-                  type={types[0]}
-                  theme="technique"
-                  disabled={taming.includes(name)}
-                  onClick={() => onClick(name)}
-                >
-                  {taming.includes(name) ? "Taming" : "Tamed?"}
-                </TemtemButton>
-              )
-            ) : null}
+              <TemtemPortrait
+                style={{ margin: "0 auto" }}
+                temtem={name}
+                shape="hexagon"
+                size={100}
+                borderWidth={5}
+              />
+              <TemtemText
+                style={{ fontSize: 20, textAlign: "center" }}
+                borderWidth={10}
+              >
+                {`#${num} ${name}`}
+              </TemtemText>
+              {jwt ? (
+                tamed.includes(name) ? (
+                  <TemtemText
+                    containerStyle={{ marginTop: 7 }}
+                    style={{ fontSize: 20 }}
+                    borderWidth={10}
+                  >
+                    Tamed
+                  </TemtemText>
+                ) : (
+                  <TemtemButton
+                    size="small"
+                    type={types[0]}
+                    theme="technique"
+                    disabled={taming.includes(name)}
+                    onClick={() => onClick(name)}
+                  >
+                    {taming.includes(name) ? "Taming" : "Tamed?"}
+                  </TemtemButton>
+                )
+              ) : null}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
