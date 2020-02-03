4484abf186dd963fd1195af6ac81284c750a0af4
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-03 08:53:17 +0000

    feat: 🎸 Implement DB for tracked quests GET

--- util/db/quests.ts
+++ util/db/quests.ts
@@ -1,3 +1,23 @@
-export function getUserQuests(_userId: string) {
-  return [];
+import client from "./client";
+import { TrackedQuest } from "../../types/db";
+
+export async function getUserQuests(
+  userId: string
+): Promise<{ data: TrackedQuest[] }> {
+  const query = `
+    query UserTrackedQuest ($userId: ID!){
+      userTrackedQuest(userId: $userId) {
+        data {
+          _id
+          userId
+          questName
+          isActive
+          createdAt
+          updatedAt
+          deletedAt
+        }
+      }
+    }
+  `;
+  return (await client.request(query, { userId })).userTrackedQuest;
 }
