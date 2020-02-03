e7266d757d7654a6d903f8c020e908062dac7f20
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-03 23:09:12 +0000

    fix: 🐛 Make NavItems look better when text stacks

--- components/primitives/NavItem.tsx
+++ components/primitives/NavItem.tsx
@@ -36,6 +36,7 @@ export default function NavItem({
           "&:hover": {
             borderColor: colors.uiBlue
           },
+          textAlign: "center",
           ...style
         }}
       >
