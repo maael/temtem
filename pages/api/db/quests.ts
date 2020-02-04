d293a3ee544be62d1e364f103f848b63d7b61ac2
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-04 23:47:23 +0000

    fix: 🐛 Add quest api changes

--- pages/api/db/quests.ts
+++ pages/api/db/quests.ts
@@ -1,28 +1,29 @@
 import cookies from "../../../util/cookies";
+import jwtGuard from "../../../util/middlewares/jwtGuard";
+import paramGuard from "../../../util/middlewares/paramGuard";
 import { getUserQuests, createUserQuests } from "../../../util/db";
 
-export default cookies(async function(req, res) {
-  const userJWT = await req.getJWT();
-  if (!userJWT) {
-    return res.status(400).json({ error: "not authenticated" });
-  }
-  if (!req.query.userId) {
-    return res.status(400).json({ error: "missing param" });
-  }
-  if (req.method === "GET") {
-    res.json(await getUserQuests(userJWT._id));
-  } else if (req.method === "POST") {
-    res.json(
-      await createUserQuests({
-        userId: userJWT._id,
-        questName: req.body.questName,
-        questStarted: false,
-        questFinished: false
-      })
-    );
-  } else if (req.method === "DELETE") {
-    res.json({ error: "not-implemented" });
-  } else {
-    res.json({ error: "not-implemented" });
-  }
-});
+export default paramGuard(
+  cookies(
+    jwtGuard(async function(req, res) {
+      const userJWT = await req.getJWT();
+      if (req.method === "GET") {
+        res.json(await getUserQuests(userJWT._id));
+      } else if (req.method === "POST") {
+        res.json(
+          await createUserQuests({
+            userId: userJWT._id,
+            questName: req.body.questName,
+            questStarted: false,
+            questFinished: false
+          })
+        );
+      } else if (req.method === "DELETE") {
+        res.json({ error: "not-implemented" });
+      } else {
+        res.json({ error: "not-implemented" });
+      }
+    })
+  ),
+  ["userId"]
+);
