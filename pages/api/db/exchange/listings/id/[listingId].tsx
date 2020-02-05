2412db669d2d67ff88d1d61a8ad2419363c70b96
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-05 11:44:47 +0000

    fix: 🐛 Don't require jwt for viewing listings

--- pages/api/db/exchange/listings/id/[listingId].tsx
+++ pages/api/db/exchange/listings/id/[listingId].tsx
@@ -8,30 +8,36 @@ import {
 } from "../../../../../../util/db";
 
 export default paramGuard(
-  cookies(
-    jwtGuard(async function(req, res) {
-      const userJWT = await req.getJWT();
-      if (req.method === "GET") {
-        const listing = await getExchangeListing(req.query.listingId);
-        res.json(listing);
-      } else if (req.method === "PUT") {
-        res.json(
-          await updateExchangeListing(req.query.listingId, {
-            ...req.body,
-            userId: userJWT._id
-          })
-        );
-      } else if (req.method === "DELETE") {
-        res.json(
-          await setExchangeInactive(req.query.listingId, {
-            ...req.body,
-            userId: userJWT._id
-          })
-        );
-      } else {
-        res.json({ error: "not-implemented" });
+  cookies(async function(req, res) {
+    const userJWT = await req.getJWT();
+    if (req.method === "GET") {
+      const listing = await getExchangeListing(req.query.listingId);
+      res.json(listing);
+    } else if (req.method === "PUT") {
+      if (!userJWT) {
+        res.status(400).json({ error: "missing jwt" });
+        return;
       }
-    })
-  ),
+      res.json(
+        await updateExchangeListing(req.query.listingId, {
+          ...req.body,
+          userId: userJWT._id
+        })
+      );
+    } else if (req.method === "DELETE") {
+      if (!userJWT) {
+        res.status(400).json({ error: "missing jwt" });
+        return;
+      }
+      res.json(
+        await setExchangeInactive(req.query.listingId, {
+          ...req.body,
+          userId: userJWT._id
+        })
+      );
+    } else {
+      res.json({ error: "not-implemented" });
+    }
+  }),
   ["listingId"]
 );
