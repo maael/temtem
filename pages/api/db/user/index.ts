c7080570c1100251b2b36986c41444b88c6e9a74
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-05 09:42:30 +0000

    fix: 🐛 Remove cookie req from users api route

--- pages/api/db/user/index.ts
+++ pages/api/db/user/index.ts
@@ -1,15 +1,10 @@
-import cookies from "../../../../util/cookies";
 import { getUsers } from "../../../../util/db";
 
-export default cookies(async function(req, res) {
-  const userJWT = await req.getJWT();
-  if (!userJWT) {
-    return res.status(400).json({ error: "not authenticated" });
-  }
+export default async function(req, res) {
   if (req.method === "GET") {
     const users = await getUsers();
     res.json(users || { data: [] });
   } else {
     res.json({ error: "not-implemented" });
   }
-});
+}
