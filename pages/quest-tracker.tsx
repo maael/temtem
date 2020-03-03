060bd542ee5769005e55a7763c7e38c0a882dbdd
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-03 23:03:42 +0000

    feat: 🎸 Improve quest tracker, add undo, add locations

--- pages/quest-tracker.tsx
+++ pages/quest-tracker.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { jsx } from "@emotion/core";
 import { IoIosLink } from "react-icons/io";
+import { MdLocationOn } from "react-icons/md";
 import TemtemText from "@maael/temtem-text-component";
 import TemtemButton from "@maael/temtem-button-component";
 import TemtemInput from "@maael/temtem-input-component";
@@ -156,7 +157,11 @@ function MainQuestItem({
             style={{ fontSize: 30, textAlign: "left" }}
             borderWidth={10}
           >
-            {isFirstMainQuest || userIsOnQuest ? q.name : "[Hidden Quest]"}
+            {isFirstMainQuest ||
+            (userIsOnQuest &&
+              (userQuest.questStarted || userQuest.questStep > 0))
+              ? q.name
+              : "[Hidden Quest]"}
           </TemtemText>
           <TemtemText
             containerStyle={{ marginLeft: 12 }}
@@ -176,15 +181,38 @@ function MainQuestItem({
             borderWidth={10}
           >
             {isFirstMainQuest || userIsOnQuest
-              ? `Step ${userQuest.questStep + 1}. ${
-                  q.steps[userQuest.questStep]
-                }` || "???"
+              ? `Step ${userQuest.questStep + 1}. ${q.steps[
+                  userQuest.questStep
+                ] || "???"}`
               : "[Hidden Step]"}
           </TemtemText>
         ) : null}
       </div>
       {isFirstMainQuest || userIsOnQuest ? (
         <RequireAuth>
+          {userQuest.questStarted &&
+          !userQuest.questFinished &&
+          userQuest.questStep > 0 ? (
+            <TemtemButton
+              onClick={async () => {
+                if (userQuestsNames.includes(q.name)) {
+                  const newStep = (userQuest.questStep || 0) - 1;
+                  await updateUserQuest({
+                    body: JSON.stringify({
+                      ...userQuest,
+                      questStep: newStep,
+                      questFinished: false
+                    })
+                  });
+                }
+                await refetchUserQuests();
+              }}
+              size="small"
+              style={{ marginRight: 5 }}
+            >
+              Undo
+            </TemtemButton>
+          ) : null}
           <TemtemButton
             onClick={async () => {
               if (userQuestsNames.includes(q.name)) {
@@ -283,9 +311,52 @@ function SideQuestItem({
             {`Step ${userQuest.questStep + 1}. ${q.steps[userQuest.questStep] ||
               "???"}`}
           </TemtemText>
+        ) : !userQuest.questFinished ? (
+          <div
+            style={{
+              marginLeft: 24,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center"
+            }}
+          >
+            <MdLocationOn size={20} />
+            <TemtemText
+              containerStyle={{ marginLeft: 8 }}
+              style={{ fontSize: 20, textAlign: "left" }}
+              borderWidth={10}
+            >
+              {`${q.startingNPC}${
+                q.startingNPC && q.startingLocation ? " in " : ""
+              }${q.startingLocation.replace("Narwhal", "The Narwhal")}`}
+            </TemtemText>
+          </div>
         ) : null}
       </div>
       <RequireAuth>
+        {userQuest.questStarted &&
+        !userQuest.questFinished &&
+        userQuest.questStep > 0 ? (
+          <TemtemButton
+            onClick={async () => {
+              if (userQuestsNames.includes(q.name)) {
+                const newStep = (userQuest.questStep || 0) - 1;
+                await updateUserQuest({
+                  body: JSON.stringify({
+                    ...userQuest,
+                    questStep: newStep,
+                    questFinished: false
+                  })
+                });
+              }
+              await refetchUserQuests();
+            }}
+            size="small"
+            style={{ marginRight: 5 }}
+          >
+            Undo
+          </TemtemButton>
+        ) : null}
         <TemtemButton
           onClick={async () => {
             if (userQuestsNames.includes(q.name)) {
