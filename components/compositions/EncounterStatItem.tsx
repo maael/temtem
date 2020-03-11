58c659fdc43b6177805c40b38b872535a63da5e9
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-11 19:03:29 +0000

    feat: 🎸 Add basic stats page

---
+++ components/compositions/EncounterStatItem.tsx
@@ -0,0 +1,35 @@
+import TemtemPortrait from "@maael/temtem-portrait-component";
+import TemtemText from "@maael/temtem-text-component";
+
+export default function EncounterStatItem({
+  temtemName,
+  count
+}: {
+  temtemName: string;
+  count: number;
+}) {
+  return (
+    <div css={{ display: "inline-block", margin: 20 }}>
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <TemtemPortrait
+          style={{ display: "inline-block", marginTop: 5, marginRight: 10 }}
+          temtem={temtemName}
+          shape="hexagon"
+          borderWidth={5}
+          size={60}
+        />
+        <TemtemText
+          borderWidth={10}
+          style={{ fontSize: 20 }}
+        >{`${count}`}</TemtemText>
+      </div>
+    </div>
+  );
+}
--- pages/encounter-tracker/stats.tsx
+++ pages/encounter-tracker/stats.tsx
@@ -1,9 +1,51 @@
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
+import Loading from "../../components/primitives/Loading";
 import EncounterTrackerHeaderBar from "../../components/compositions/EncounterTrackerHeaderBar";
+import EncounterStatItem from "../../components/compositions/EncounterStatItem";
+import useFetch from "../../components/hooks/useFetch";
 
-export default () => {
+export default function EncounterStatsPage({ user }) {
+  const [encounters, loadingEncounters] = useFetch<Record<string, number>>(
+    `/db/encounters/user/${user._id}`,
+    {},
+    {
+      source: "local",
+      defaultValue: {},
+      mapper: ({ data }) =>
+        data.reduce(
+          (acc, { temtemName }) => ({
+            ...acc,
+            [temtemName]: Number(acc[temtemName] || 0) + 1
+          }),
+          {}
+        )
+    }
+  );
   return (
     <>
       <EncounterTrackerHeaderBar />
+      <Loading loading={loadingEncounters} />
+      <div css={{ textAlign: "center", maxWidth: 1000, margin: "0px auto" }}>
+        {Object.entries(encounters).map(([temtemName, count]) => (
+          <EncounterStatItem
+            key={temtemName}
+            temtemName={temtemName}
+            count={count}
+          />
+        ))}
+      </div>
     </>
   );
+}
+
+EncounterStatsPage.getInitialProps = async ({ req }) => {
+  try {
+    const cookieString = req ? req.headers.cookie : document.cookie;
+    const parsed = cookie.parse(cookieString)["temtem-jwt"];
+    const decoded = jwt.decode(parsed, { json: true });
+    return { user: decoded };
+  } catch (e) {
+    return { user: {} };
+  }
 };
