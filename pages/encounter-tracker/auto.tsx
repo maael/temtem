8e55a172532c140f459ec088a0d47f2626bda187
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-12 07:33:53 +0000

    feat: 🎸 Add better auto tracker snapshots

--- pages/encounter-tracker/auto.tsx
+++ pages/encounter-tracker/auto.tsx
@@ -1,9 +1,21 @@
-import { createRef } from "react";
+import { createRef, useRef } from "react";
 import { colors } from "@maael/temtem-theme";
 import EncounterTrackerHeaderBar from "../../components/compositions/EncounterTrackerHeaderBar";
 
+declare const ImageCapture: any;
+
 export default () => {
   const videoRef = createRef<HTMLVideoElement>();
+  const videoOverlayRef = createRef<HTMLCanvasElement>();
+  const rafRef = useRef<number>();
+  const imageCapture = useRef<typeof ImageCapture>();
+  async function fn() {
+    if (videoRef.current && imageCapture.current) {
+      const imageBitmap = await imageCapture.current.grabFrame();
+      console.info(imageBitmap.height, imageBitmap.width);
+    }
+    rafRef.current = requestAnimationFrame(fn);
+  }
   return (
     <>
       <EncounterTrackerHeaderBar />
@@ -15,14 +27,28 @@ export default () => {
             marginBottom: 10
           }}
         >
-          <video
-            ref={videoRef}
-            autoPlay
-            style={{
-              background: colors.uiBlueFaded,
-              width: "30vw"
-            }}
-          />
+          <div css={{ position: "relative" }}>
+            <video
+              ref={videoRef}
+              autoPlay
+              style={{
+                background: colors.uiBlueFaded,
+                width: "50vw"
+              }}
+            />
+            <canvas
+              ref={videoOverlayRef}
+              style={{
+                position: "absolute",
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                height: "calc(100% - 6px)",
+                width: "100%"
+              }}
+            />
+          </div>
         </div>
         <div
           style={{
@@ -37,7 +63,7 @@ export default () => {
                 if (!videoRef.current) return;
 
                 try {
-                  videoRef.current.srcObject = await (navigator.mediaDevices as any).getDisplayMedia(
+                  const stream = await (navigator.mediaDevices as any).getDisplayMedia(
                     {
                       video: {
                         cursor: "never"
@@ -45,6 +71,10 @@ export default () => {
                       audio: false
                     }
                   );
+                  videoRef.current.srcObject = stream;
+                  const track = stream.getVideoTracks()[0];
+                  imageCapture.current = new ImageCapture(track);
+                  rafRef.current = requestAnimationFrame(fn);
                 } catch (err) {
                   console.error(`Error: ${err.message}`);
                 }
@@ -55,6 +85,7 @@ export default () => {
           </button>
           <button
             onClick={() => {
+              if (rafRef.current) cancelAnimationFrame(rafRef.current);
               if (videoRef.current && videoRef.current.srcObject) {
                 (videoRef.current.srcObject as any).getTracks()[0].stop();
                 videoRef.current.srcObject = null;
