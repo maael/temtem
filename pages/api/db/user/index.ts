f6e6c8b92994c78c69efa84ab763c49006b7033a
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-18 18:42:12 +0000

    fix: 🐛 Linting issue

--- pages/api/db/user/index.ts
+++ pages/api/db/user/index.ts
@@ -9,7 +9,7 @@ export default cookies(async function(req, res) {
     const userJWT = await req.getJWT();
     if (!userJWT) {
       res.status(400).json({ error: "not authenticated" });
-      return undefined;
+      return;
     }
     const existing = await getUser(userJWT._id);
     res.json(
