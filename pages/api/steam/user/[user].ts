2ee80f17f47af607d82fc335debb4a49dacb47f3
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-03-18 22:25:53 +0000

    fix: 🐛 Handle errors in steam user route better

--- pages/api/steam/user/[user].ts
+++ pages/api/steam/user/[user].ts
@@ -7,23 +7,27 @@ export default async function steamUser(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const isSteamId =
-    req.query.user.length === 17 && !isNaN(Number(req.query.user));
-  let steamId = req.query.user;
-  if (!steamId) {
-    res.status(400).json({ error: "Missing steam ID" });
-    return;
-  }
-  if (!isSteamId) {
-    const fres = await fetch(
-      `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_KEY}&vanityurl=${req.query.user}`
+  try {
+    const isSteamId =
+      req.query.user.length === 17 && !isNaN(Number(req.query.user));
+    let steamId = req.query.user;
+    if (!steamId) {
+      res.status(400).json({ error: "Missing steam ID" });
+      return;
+    }
+    if (!isSteamId) {
+      const fres = await fetch(
+        `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_KEY}&vanityurl=${req.query.user}`
+      );
+      const json = await fres.json();
+      steamId = json.response.steamid;
+    }
+    const fres2 = await fetch(
+      `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_KEY}&steamids=${steamId}`
     );
-    const json = await fres.json();
-    steamId = json.response.steamid;
+    const json2 = await fres2.json();
+    res.json(json2.response.players[0] || {});
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
-  const fres2 = await fetch(
-    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_KEY}&steamids=${steamId}`
-  );
-  const json2 = await fres2.json();
-  res.json(json2.response.players[0] || {});
 }
